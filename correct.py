ae73457db46e537d45ea96af2180293beaf3161c
akinsiraifedayo <akinsiraolympicson@gmail.com>
2023-09-03 13:19:16 -0400

    before running auto template script

--- correct.py
+++ correct.py
@@ -1,6 +1,47 @@
 from bs4 import BeautifulSoup
 import os
 
+def custom_prettyprint(html):
+    lines = html.splitlines()
+    result = []
+    indent_level = 0
+
+    for line in lines:
+        if any(tag in line for tag in ['<html>', '<head>', '<body>', '<p>', '<ul>', '<li>', '<a>', '<i>', '<div>']):
+            result.append(" " * indent_level * 2 + line)
+            indent_level += 1
+        else:
+            if any(tag in line for tag in ['</html>', '</head>', '</body>', '</p>', '</ul>', '</li>', '</a>', '</i>', '</div>']):
+                indent_level -= 1
+            result.append(" " * indent_level * 2 + line)
+
+    return "\n".join(result)
+
+def soup_prettify2(soup, desired_indent): #where desired_indent is number of spaces as an int() 
+	pretty_soup = str()
+	previous_indent = 0
+	for line in soup.prettify().split("\n"): # iterate over each line of a prettified soup
+		current_indent = str(line).find("<") # returns the index for the opening html tag '<' 
+		# which is also represents the number of spaces in the lines indentation
+		if current_indent == -1 or current_indent > previous_indent + 2:
+			current_indent = previous_indent + 1
+			# str.find() will equal -1 when no '<' is found. This means the line is some kind 
+			# of text or script instead of an HTML element and should be treated as a child 
+			# of the previous line. also, current_indent should never be more than previous + 1.	
+		previous_indent = current_indent
+		pretty_soup += write_new_line(line, current_indent, desired_indent)
+	return pretty_soup
+		
+		
+def write_new_line(line, current_indent, desired_indent):
+	new_line = ""
+	spaces_to_add = (current_indent * desired_indent) - current_indent
+	if spaces_to_add > 0:
+		for i in range(spaces_to_add):
+			new_line += " "		
+	new_line += str(line) + "\n"
+	return new_line
+
 # Function to process an HTML file
 def process_html_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -16,9 +57,12 @@ def process_html_file(file_path):
     # Save the modified content back to the file
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(soup.prettify())
+        # file.write(soup_prettify2(soup, desired_indent=4))
+
+
 
 # Specify the directory containing your HTML files
-directory_path = 'C:\\Users\\USER\\Videos\\Code\\All_Personal_Jobs\\Eyop Logistics Flask\\templates'
+directory_path = 'C:\\Users\\USER\\Videos\\Code\\All_Personal_Jobs\\Eyop Logistics Flask\\temptest'
 
 # Process all HTML files in the directory
 for filename in os.listdir(directory_path):
--- main.py
+++ main.py
@@ -83,6 +83,56 @@ def admins_only(func):
             return abort(403)
     return decorated_func
 
+# In your Flask route function or context processor
+navigation_items = [
+    {"label": "Home", "url_for": "home", "sub_items": None},
+    {
+        "label": "Pages",
+        "url_for": None,  # No direct link for the parent item
+        "sub_items": [
+            {"label": "About Us", "url_for": "about"},
+            {"label": "Pricing", "url_for": "pricing"},
+            {"label": "Testimonials", "url_for": "testimonials"},
+            {"label": "Our Team", "url_for": "team"},
+            # Add more sub-items as needed
+        ],
+    },
+    {
+        "label": "Shop",
+        "url_for": None,  # No direct link for the parent item
+        "sub_items": [
+            {"label": "Products", "url_for": "products"},
+            {"label": "Cart", "url_for": "cart"},
+            {"label": "Checkout", "url_for": "checkout"},
+            {"label": "Product Details", "url_for": "product_details"},
+            {"label": "Global Location", "url_for": "global_location"},
+            {"label": "FAQ", "url_for": "faq"},
+            {"label": "Privacy Policy", "url_for": "privacy_policy"},
+            {"label": "Terms and Conditions", "url_for": "terms_and_conditions"},
+            # Add more sub-items as needed
+        ],
+    },
+    {"label": "Services", "url_for": "services", "sub_items": None},
+    {"label": "Blog", "url_for": "blog", "sub_items": None},
+    {"label": "Company", "url_for": "company", "sub_items": None},
+    {"label": "Contact Us", "url_for": "contact_us", "sub_items": None},
+    {
+        "label": "User",
+        "url_for": None,  # No direct link for the parent item
+        "sub_items": [
+            {"label": "My Account", "url_for": "my_account"},
+            {"label": "Log In", "url_for": "login"},
+            {"label": "Register", "url_for": "register"},
+            {"label": "Forgot Password", "url_for": "recover_password"},
+            # Add more sub-items as needed
+        ],
+    },
+    # Add more navigation items as needed
+]
+@app.context_processor
+def inject_current_page():
+    current_page = request.endpoint
+    return dict(current_page=current_page, navigation_items=navigation_items)
 
 
 
@@ -108,6 +158,7 @@ def coming_soonest():
             return redirect(url_for("coming_soonest"))
             
     return render_template("coming-soon-copy.html", subscribed=subscribed)
+
 
 
 @app.route('/')
